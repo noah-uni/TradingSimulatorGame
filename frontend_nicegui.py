f62c89a846bdebd4463b2f0ff1416bb08aa7bff9
noah-uni <s231870@student.dhbw-mannheim.de>
2024-07-04 18:58:56 +0200

    small frontend changes

--- frontend_nicegui.py
+++ frontend_nicegui.py
@@ -6,5 +6,11 @@ Data = Game.get_stock_prices("EUR/USD", "2022-08-09 07:20", "2022-08-10 00:00")
 
 from nicegui import ui
 
-ui.label("Hello")
+with ui.dropdown_button('Select Ticker', auto_close=True):
+    ui.item('EUR/USD', on_click=lambda: ui.notify('You clicked item 1'))
+    ui.item('BTC/USD', on_click=lambda: ui.notify('You clicked item 2'))
+
+select_time = ui.select(["1min", "15min", "1h", "4h"], with_input=True, value=1)
+select_charttype = ui.select(["Candles", "Line"], with_input=True, value=1)
+
 ui.run()
